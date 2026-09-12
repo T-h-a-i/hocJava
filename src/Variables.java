d5b814446817ae2295096527bf5430acf42e095a
Tran Van Thai <tranthai11082007@gmail.com>
2026-09-12 10:53:01 +0700

    hoc bien so

---
+++ src/Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String[] args) {
+        int Thai = 26;
+        int test;
+        test = 6;
+        int a = 2;
+        int sum = (Thai + test - a) * a / test;
+        System.out.println("Sum: " + sum);
+    }
+}
