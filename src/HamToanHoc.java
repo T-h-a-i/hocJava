34d48840ddaca0edbd0e732c9b698b05ebe19a02
Tran Van Thai <tranthai11082007@gmail.com>
2026-09-13 00:37:31 +0700

    ham toan hoc

---
+++ src/HamToanHoc.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class HamToanHoc {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("First number: ");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Second number: ");
+        int secondNumber = scanner.nextInt();
+        System.out.println("Min = " + Math.min(firstNumber, secondNumber));
+        scanner.close();
+
+        System.out.println("Hàm mũ: 2^3 = " + Math.pow(2, 3));
+        System.out.println("Hàm căn bậc 2: căn bậc hai của 25 = " + Math.sqrt(25));
+    }
+}
