878b4b75636fe366254b2e9859c4e09416b22d07
Tran Van Thai <tranthai11082007@gmail.com>
2026-09-21 22:11:11 +0700

    giới thiệu về OOP

---
+++ src/Lab04/OOP.java
@@ -0,0 +1,16 @@
+package Lab04;
+
+public class OOP {
+    public static void main(String[] args) {
+        // OOP là cách chúng ta mô phỏng thế giới thực vào chương trình máy tính.
+        // tất cả các "đối tượng" tham gia vào chương chình chính là những thành phần chủ chốt.
+        // VD: Website hoidanit.com.vn(phân tích chức năng khóa học):
+        // Các "đối tượng"/thành phần tạo nên website:
+        //     + Danh sách khóa học
+        //          + Khóa học riêng lẻ
+        //              + Nội dung khóa học: tiêu đề, giá cả...
+        //              + Giáo án khóa học
+        // => OOP thiên hướng "mô phỏng" những điều bạn thấy(sử dụng) ở thế giới thực vào thế giới coding
+    }
+}
+
